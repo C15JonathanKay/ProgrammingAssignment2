cf207a23b64a99485bbe04db415da83baa67eca6
Jonathan Kay <c15jonathan.kay@usafa.edu>
2015-01-24 11:19:21 -0700

    Programming Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Solve and cache inverse of square matrix
+## C15JonathanKay
 
-## Write a short comment describing this function
+## Initializes funciton used to cache. Obtains raw matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+  J <- NULL
+  G <- function() x
+  setImatrix <- function(Imatrix) J <<- Imatrix
+  getImatrix <- function() J
+  
+  # return a list of functions as an R object
+  list(G=G, setImatrix=setImatrix, getImatrix=getImatrix)
 }
 
 
-## Write a short comment describing this function
+## Checks to see that the matrix is nonsingular. If it is, returns value, caches, and exits.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  J <- x$getImatrix()
+  if(!is.null(J)){
+    message("Cached data found. Getting result... Done.")
+    return(J)
+  }
+  else {
+    message("No cached data found. Calculating inverse matrix...")
+    data <- x$get() # obtains matrix from object x
+    J <- solve(data) # finds inverse matrix
+    x$setImatrix(J) # assigns resulting inverse matrix to object x
+    return(J)
+  }
 }
